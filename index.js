a62b566bf0c488c33ab93d799f193a9c624a23dc
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2023-07-12 17:18:08 -0300

    comportamento de classes

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrentDoRicardo = new ContaCorrente();
+contaCorrentDoRicardo.saldo = 0;
+contaCorrentDoRicardo.agencia = 1001
 
 console.log(cliente1);
 console.log(cliente2);
